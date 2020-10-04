9574a610ecb0fd2771e2f49584118d845be7f868
adinh <adinh254@gmail.com>
2020-10-04 02:24:59 -0700

    fixed a bunch of shit

---
+++ application/default_settings.py
@@ -0,0 +1,11 @@
+# When behind a load balancer, set CANONICAL_NAME to the value contained in
+# Host headers (e.g. 'www.example.org')
+CANONICAL_NAME = '127.0.0.1'
+
+DATABASE = 'tmp/CPSC449.db'
+
+DB_HOST = '127.0.0.1'
+DB_NAME = 'CPSC449_Database'
+
+TESTING = False
+DEBUG = False
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='CPSC449-Project2',
+    version='1.0',
+    author='Andrew Dinh',
+    packages=['application'],
+    include_package_data=True,
+    install_requires=['Flask']
+)
