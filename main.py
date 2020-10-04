4375255a492701faf4ee1f7f1fdd72748ed907d8
adinh <adinh254@gmail.com>
2020-10-03 21:02:30 -0700

    test

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sqlite3
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.cli.command('init')
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('books.sql', mode='r') as f:
+            db.cursor().execute
+
 
 if __name__ == '__main__':
     app.run()
