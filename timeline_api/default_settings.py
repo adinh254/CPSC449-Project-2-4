2c143f75224bb3df53d868a20dbe8f682f73f11a
adinh <adinh254@gmail.com>
2020-10-07 18:16:00 -0700

    split both api for foreman use

--- timeline_api/default_settings.py
+++ timeline_api/default_settings.py
@@ -1,11 +1,10 @@
 # When behind a load balancer, set CANONICAL_NAME to the value contained in
 # Host headers (e.g. 'www.example.org')
-CANONICAL_NAME = '127.0.0.1'
+FLASK_RUN_HOST='127.0.0.1'
+FLASK_RUN_PORT='5001'
 
-DATABASE = 'tmp/CPSC449.db'
-
-DB_HOST = '127.0.0.1'
-DB_NAME = 'CPSC449_Database'
+DATABASE='tmp/CPSC449.db'
+DB_NAME='CPSC449_Database'
 
 TESTING = False
 DEBUG = False
---
+++ user_api/default_settings.py
@@ -0,0 +1,10 @@
+# When behind a load balancer, set CANONICAL_NAME to the value contained in
+# Host headers (e.g. 'www.example.org')
+FLASK_RUN_HOST='127.0.0.1'
+FLASK_RUN_PORT='5000'
+
+DATABASE='tmp/CPSC449.db'
+DB_NAME='CPSC449_Database'
+
+TESTING = False
+DEBUG = False
